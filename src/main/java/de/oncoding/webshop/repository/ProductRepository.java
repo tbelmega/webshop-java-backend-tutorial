e8ee6740f37136c6f910fd1a1c226be5a1f4dfee
TotalSurprise <pardertec@gmail.com>
2020-10-24 19:34:13 -0700

    Extract method lowercaseTags

--- src/main/java/de/oncoding/webshop/repository/ProductRepository.java
+++ src/main/java/de/oncoding/webshop/repository/ProductRepository.java
@@ -8,32 +8,31 @@ import java.util.List;
 
 public class ProductRepository {
 
+    List<ProductResponse> products = Arrays.asList(
+            new ProductResponse(
+                    "1",
+                    "AMD Ryzen 9 5950X",
+                    "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
+                    79900,
+                    Arrays.asList("AMD", "Processor")
+            ),
+            new ProductResponse(
+                    "2",
+                    "Intel Core i9-9900KF",
+                    "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
+                    37900,
+                    Arrays.asList("Intel", "Processor")
+            ),
+            new ProductResponse(
+                    "3",
+                    "NVIDIA GeForce GTX 1080 Ti Black Edition 11GB",
+                    "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
+                    74900,
+                    Arrays.asList("NVIDIA", "graphics")
+            )
+    );
+
     public List<ProductResponse> findAll(String tag){
-
-        List<ProductResponse> products = Arrays.asList(
-                new ProductResponse(
-                        "1",
-                        "AMD Ryzen 9 5950X",
-                        "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
-                        79900,
-                        Arrays.asList("AMD", "Processor")
-                ),
-                new ProductResponse(
-                        "2",
-                        "Intel Core i9-9900KF",
-                        "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
-                        37900,
-                        Arrays.asList("Intel", "Processor")
-                ),
-                new ProductResponse(
-                        "3",
-                        "NVIDIA GeForce GTX 1080 Ti Black Edition 11GB",
-                        "grsehz dt rfz jzjs j tjzfgk gjse rzj6drj sej",
-                        74900,
-                        Arrays.asList("NVIDIA", "graphics")
-                )
-        );
-
 
         if (tag == null)
             return products;
@@ -44,16 +43,19 @@ public class ProductRepository {
 
             for (ProductResponse p : products) {
 
-                List<String> lowercaseTags = new ArrayList<>();
-                for (String t : p.getTags()) {
-                    lowercaseTags.add(t.toLowerCase());
-                }
-
-                if (lowercaseTags.contains(tag))
+                if (lowercaseTags(p).contains(tag))
                     filtered.add(p);
             }
             return filtered;
         }
+    }
+
+    private List<String> lowercaseTags(ProductResponse p) {
+        List<String> lowercaseTags = new ArrayList<>();
+        for (String t : p.getTags()) {
+            lowercaseTags.add(t.toLowerCase());
+        }
+        return lowercaseTags;
     }
 
 }
