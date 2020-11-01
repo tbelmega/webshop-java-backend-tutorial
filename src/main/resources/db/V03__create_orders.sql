694be6615e9eb63c6a1007ba485ddb8733a0ef79
TotalSurprise <pardertec@gmail.com>
2020-11-01 10:16:26 -0800

    Create database migration scripts

---
+++ src/main/resources/db/V03__create_orders.sql
@@ -0,0 +1,30 @@
+create table order_positions (
+        id varchar(36)  not null,
+        order_id varchar(36) not null,
+        product_id varchar(36)  not null,
+        quantity bigint not null
+    );
+
+create table orders (
+       id varchar(36) not null,
+        customer_id varchar(36)  not null,
+        order_time timestamp  not null,
+        status varchar(255)  not null,
+        primary key (id)
+    );
+
+alter table order_positions
+       add constraint order_positions_order_id_fk
+       foreign key (order_id)
+       references orders;
+
+alter table order_positions
+       add constraint order_positions_product_id_fk
+       foreign key (product_id)
+       references products;
+
+alter table orders
+       add constraint orders_customer_id_fk
+       foreign key (customer_id)
+       references customers;
+
